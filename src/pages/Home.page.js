56dd06cb0fc62d7a5bad9d22dd1fdf549f5ba042
jjcrl <j412cff@icloud.com>
2022-08-18 14:07:24 +0100

    minor changes to home

--- src/pages/Home.page.js
+++ src/pages/Home.page.js
@@ -7,28 +7,47 @@ export default function Home() {
   return (
     <>
       <Hero />
-      <WordsSection />
+      <WordsSection
+        section1="Let's say hello to section one together."
+        section2="Who could forget aout section 2? Not me thats for sure."
+        section3="This is the small but impactful section 3, it is usually aliitle bit bigger than the next section."
+        section4="And last but never least we have section 4, the smallest of all."
+        background={"white"}
+        font1="pinkflame"
+        font2="ink"
+      />
       <ShowCase
-        works={[
+        items={[
           {
-            tags: [1, 2, 3],
+            tags: ["a", "b", "c"],
             title: "test1",
-            words:
+            description:
               "theese are some words aboutt the project, it has this that and is so good and and so amazing",
             link: "www.google.com",
-            image: "test",
+            image: "pinkflame",
+            theme: "pinkflame",
+            id: "showcase-item-0",
           },
           {
-            tags: [1, 2, 3],
+            tags: ["a", "b", "c"],
             title: "test2",
-            words:
+            description:
               "theese are some words aboutt the project, it has this that and is so good and and so amazing",
             link: "www.google.com",
-            image: "test",
+            image: "boyblue",
+            theme: "boyblue",
+            id: "showcase-item-1",
           },
         ]}
       />
-      <WordsSection />
+      <WordsSection
+        section2="This is the second section."
+        section3="This is just a mini, a smaller section, but not the smallest one yet."
+        section4="And this is the final section, the smallest of all."
+        background={"ink"}
+        font1={"road"}
+        font2={"pinkflame"}
+      />
       <Footer />
     </>
   );
