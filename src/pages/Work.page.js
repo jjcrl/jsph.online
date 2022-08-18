c934dc21c701ff33c71116b7002835c643817f40
jjcrl <j412cff@icloud.com>
2022-08-18 14:07:08 +0100

    works pages update

--- src/pages/Work.page.js
+++ src/pages/Work.page.js
@@ -1,37 +1,72 @@
 import Footer from "../../components/footer";
+import Topper from "../../components/topper";
 import WordsSection from "../../components/words-section";
+import forecastface from "../../public/images/forecastface.png";
+import Image from "next/image";
+
 import Link from "next/link";
+import Gallery from "../../components/gallery";
 export default function Work() {
   return (
     <>
+      <WordsSection
+        section2="Section Two, for more things that is spereate from 3 and 4."
+        section3="Section Three, this is togther with section 4."
+        section4="Section Four, this is the final section."
+        background={"ink"}
+        font2={"pinkflame"}
+      />
       <section data-scroll-section className="flex flex-col">
-        <WordsSection />
-        <Link href={"/work/work1"}>
-          <div className="h-screen w-screen text-white" id="works-1">
-            <div className="show">
-              <h3>title</h3>
-              <h4>line</h4>
+        <div data-scroll>
+          <Link href={"/work/work1"}>
+            <div className="h-screen w-screen bg-white" id="works">
+              <div className="show">
+                <div className="absolute inset-0 w-screen h-screen z-0">
+                  <Image src={forecastface} />
+                </div>
+                <h3>Title</h3>
+                <h4 className="italic">line</h4>
+              </div>
             </div>
-          </div>
-        </Link>{" "}
-        <Link href={"/work/work2"}>
-          <div className="h-screen w-screen text-white opacity-0	" id="works-2">
-            <div className="show">
-              <h3>title</h3>
-              <h4>line</h4>
+          </Link>{" "}
+          {/* <Link href={"/work/work2"}>
+            <div className="h-screen w-screen bg-white" id="works">
+              <div className="show">
+                <h3>Title</h3>
+                <h4 className="italic">line</h4>
+              </div>
             </div>
-          </div>
-        </Link>{" "}
-        <Link href={"/work/work3"}>
-          <div className="h-screen w-screen text-white opacity-0	" id="works-3">
-            <div className="show">
-              <h3>title</h3>
-              <h4>line</h4>
+          </Link>{" "}
+          <Link href={"/work/work3"}>
+            <div className="h-screen w-screen bg-white" id="works">
+              <div className="show">
+                <h3>Title</h3>
+                <h4 className="italic">line</h4>
+              </div>
             </div>
-          </div>
-        </Link>
+          </Link> */}
+        </div>
       </section>
-      <WordsSection />
+      <WordsSection
+        section1="Section 1, main section."
+        section2="Section 2, for more things that is spereate from 3 and 4."
+        section3="Section 3, this is togther with section 4."
+        section4="Section 4, this is the final section."
+        background={"pale"}
+      />
+      <Gallery
+        items={["one,", "two", "three", "four", "five"]}
+        background="pale"
+      />
+      <WordsSection
+        section1="☺"
+        section2="Section Two, for more things that is spereate from 3 and 4."
+        section3="Section Three, this is togther with section 4."
+        section4="Section Four, this is the final section."
+        background={"ink"}
+        font1={"pinkflame"}
+        font2={"road"}
+      />
       <Footer />
 
       <style jsx>
@@ -40,14 +75,18 @@ export default function Work() {
             display: flex;
             flex-direction: column;
             justify-content: space-between;
+            flex-wrap: nowrap;
             height: 100%;
             width: 100%;
-            padding: 8em;
-            background-color: white;
+            // padding: 9em 0 1em 5em;
+            color: white;
+            align-items: start;
+            backgrounf-color: transparent;
           }
 
-          div:hover .show {
-            background-color: rgba(0, 0, 0, 0.5);
+          #works:hover .show {
+            background-color: rgba(0, 0, 0, 0.1);
+            color: white;
           }
         `}
       </style>
--- src/pages/Work1.page.js
+++ src/pages/Work1.page.js
@@ -3,20 +3,68 @@ import ShowCase from "../../components/showcase";
 import Topper from "../../components/topper";
 import WordsSection from "../../components/words-section";
 import Gallery from "../../components/gallery";
+import forecastface from "../../public/images/forecastface.png";
+import SunRingSVG from "../../components/svg/sun-ring.svg";
+import TempHandSVG from "../../components/svg/temp-hand.svg";
+import DialFaceSVG from "../../components/svg/dial-face.svg";
+import ForecastFaceFonts from "../../components/forecastface-fonts";
+import ForecastFaceColours from "../../components/forecastface-colours";
 
 export default function Work1() {
   return (
     <>
-      <Topper />
-      <WordsSection />
-      <ShowCase />
+      <Topper bg={forecastface} />
+      <WordsSection
+        section1="Forecast Face"
+        section2="24hr weather forecast at a glance."
+        section3="A Stand alone webapp for daily weather data visulaisation."
+        section4="and this could be for the guthub or link?"
+        background="black"
+        font1="pale"
+        font2="whitesmoke"
+      />
+      <ShowCase
+        items={[
+          {
+            image: <DialFaceSVG />,
+            title: "Daily Dial Infomation",
+            description:
+              "Your day at a glace, display what your need to know for right now.",
+            theme: "whitesmoke",
+            id: "showcase-item-0",
+          },
+          {
+            image: <SunRingSVG />,
+            title: "sun position tracking",
+            description:
+              "Sun rise, the highest point of the sun , and sun set, disaplyed subtley, to keep track of where the sun is thoughout the day",
+            theme: "peachpuff",
+            id: "showcase-item-1",
+          },
+          {
+            image: <TempHandSVG />,
+            title: "Current temperature, on the hour, every hour.",
+            description:
+              "As the hand moves around the dial with the time, it tracks the current temperature and siaply its about the hand.",
+            theme: "road",
+            id: "showcase-item-2",
+          },
+        ]}
+      />
       <Gallery
         direction="col"
         bg="black"
         height="96"
-        items={[{ x: 1 }, { x: 2 }, { x: 3 }]}
+        items={[<ForecastFaceFonts />, <ForecastFaceColours />]}
       />
-      <WordsSection />
+      <WordsSection
+        section1="This is  the last first section."
+        section2="And this is still the second section that could be very long on its own."
+        section3="& maybe just one section thats a mini ?"
+        background={"ink"}
+        font1={"whitesmoke"}
+        font2={"road"}
+      />
       <Footer />
     </>
   );
