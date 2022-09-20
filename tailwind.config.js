a9e2d27a07a5a7bb9a814ed127209c126b86e763
jjcrl <j412cff@icloud.com>
2022-09-20 14:17:11 +0100

    safelist for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: "jit",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
@@ -20,7 +21,6 @@ module.exports = {
           "100%": { transform: "translateX(0%)" },
         },
       },
-
       colors: {
         neoolive: "#1e3002",
         pinkflame: "#ff005d",
@@ -29,13 +29,13 @@ module.exports = {
         boyblue: "#2d6bfb",
         pale: "#f4f0f2",
         road: "#5e4d5d",
-        peachpuff: "#FFDAB9",
+        peachpuff: "#ffdaB9",
         whitesmoke: "#cfcbd9",
         blurple: "#412cff",
         cod: "#181315",
         watermelon: "#fe4b72",
-        mustard: "#D4C951",
-        dayglow: "#E95B37",
+        mustard: "#d4c951",
+        dayglow: "#e95b37",
       },
       letterSpacing: {
         superwide: "0.35em",
@@ -43,8 +43,25 @@ module.exports = {
       fontSize: {
         jumbo: "28rem",
       },
+      borderRadius: {
+        thick: "5rem",
+      },
     },
   },
+  safelist: [
+    "bg-road",
+    "bg-peachpuff",
+    "bg-whitesmoke",
+    "bg-black",
+    "bg-pale",
+    "bg-blurple",
+    "bg-watermelon",
+    "bg-cod",
+    "bg-dayglow",
+    "py-16",
+    "px-20",
+    "pb-28",
+  ],
   plugins: [],
   variants: {
     textColor: ["odd", "even"],
