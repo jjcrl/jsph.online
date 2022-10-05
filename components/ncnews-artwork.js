c6e6c28dcfb03861dac2c56f7ee2d9aca61c52e0
jjcrl <j412cff@icloud.com>
2022-10-05 16:04:06 +0100

    added re rendering for artwork change

--- components/ncnews-artwork.js
+++ components/ncnews-artwork.js
@@ -1,30 +1,20 @@
-import { useState } from "react";
 import GenerativeArtwork from "./generative-artwork";
-import GalleryGrid from "./ncnews-artwork-gallery";
 
 const Artwork = () => {
-  const [show, setShow] = useState(false);
-
-  const toggleShow = () => {
-    setShow(!show);
-  };
-
   return (
-    <div className="flex flex-row w-screen h-screen bg-pale py-20 mt-20">
-      <div className="w-3/4 py-10 pl-32 pr-10 m-auto">
-        <GenerativeArtwork height={35} border={true} />
+    <div className="flex flex-row-reverse w-screen h-screen bg-white p-20">
+      <div className="w-2/3  m-auto px-3">
+        <GenerativeArtwork height={38} border={true} tick={true} />
       </div>
 
-      <div className="w-1/3 m-auto flex flex-col mr-28">
-        <h4 className="m-5 tracking-none">Generative Grid Artwork</h4>
-        <p className="m-5 leading-normal text-xl text-cod">
+      <div className="w-1/3 m-auto flex flex-col px-5 gap-8 justify-center">
+        <h4>Generative Grid Artwork</h4>
+        <h7>
           Creative coding to genreate endless version of grid art, with the web
-          apps core colours.
-          <br />
-          <br />
-          Design to create a pefect square in the middle should the values
-          align.
-        </p>
+          apps core colours. Design to create a pefect square in the middle
+          should the values align.
+        </h7>
+        <h7>As always full coruce code can be found on my github.</h7>
       </div>
     </div>
   );
