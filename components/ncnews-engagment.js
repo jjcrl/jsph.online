af956e8d9128a05bdcfb27365f8aea2508dac09d
jjcrl <j412cff@icloud.com>
2022-10-05 16:03:41 +0100

    component added

---
+++ components/ncnews-engagment.js
@@ -0,0 +1,69 @@
+const NcNewsEngagement = () => {
+  return (
+    <div className="w-screen h-screen flex p-10 bg-white">
+      <div className="w-1/2 h-full px-16 py-5" id="system-ui">
+        <div className="w-full h-full bg-cod grid grid-cols-7 grid-rows-8 text-center items-center text-4xl text-blurple px-4">
+          <span>≡</span>
+          <span>++</span>
+          <span>≡</span>
+          <span>++</span>
+          <span>≡</span>
+          <span>++</span>
+          <span>≡</span>
+          <span>++</span>
+          <span>≡</span>
+          <span>++</span>
+          <span>≡</span>
+          <span>++</span>
+          <span>≡</span>
+          <span>++</span>
+          <span>≡</span>
+          <span>++</span>
+          <span className="row-span-3 col-span-3 text-big flex justify-center items-center font-black text-watermelon">
+            ¤
+          </span>
+          <span>++</span>
+          <span>≡</span>
+          <span>++</span>
+          <span>≡</span>
+          <span>≡</span>
+          <span>++</span>
+          <span>≡</span>
+          <span>++</span>
+          <span>++</span>
+          <span>≡</span>
+          <span>++</span>
+          <span>≡</span>
+          <span>++</span>
+          <span>≡</span>
+          <span>++</span>
+          <span>≡</span>
+          <span>++</span>
+          <span>≡</span>
+          <span>++</span>
+          <span>≡</span>
+          <span>++</span>
+          <span>≡</span>
+          <span>++</span>
+          <span>≡</span>
+        </div>
+      </div>
+      <div className="w-1/2 h-full flex flex-col gap-8 justify-center px-10">
+        <h4>Engagment Tracked</h4>
+        <h7>
+          Engagement is center for ncnews, through the site it is display in
+          many ways to highlight whats posts are taking off and what users are
+          buzzing over. It is tracked and totaled to kepe useres up to date
+          whoth what conversations are taking off.
+        </h7>
+        <h7>
+          For this symbols are used, and on first visit the user is greeted with
+          a run down on what each of them means, so that they can stay in the
+          know.
+        </h7>
+      </div>
+    </div>
+  );
+};
+
+export default NcNewsEngagement;
