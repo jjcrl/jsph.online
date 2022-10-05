23c56e3df2466c7b9267fca7d79854a54a5c8bbe
jjcrl <j412cff@icloud.com>
2022-10-05 18:23:40 +0100

    ASCII art added to hero

--- components/hero.js
+++ components/hero.js
@@ -2,9 +2,43 @@ const Hero = () => {
   return (
     <section
       data-scroll-section
-      className="flex flex-col bg-neutral-50 w-screen h-screen p-0 m-0"
+      className="flex flex-col bg-pale w-screen h-screen p-0 m-0"
     >
-      <h2 className="text-pale tracking-widest m-auto ">jsph.online</h2>
+      <pre className="leading-normal m-auto text-imnk">
+        <code>{`
+                      .,,uod8B8bou,,.
+              ..,uod8BBBBBBBBBBBBBBBBRPFT?l!i:.
+         ,=m8BBBBBBBBBBBBBBBRPFT?!||||||||||||||||
+         !...:!TVBBBRPFT||||||||||!!^^""'     ||||
+         !.......:!?|||||!!^^""''            ||||
+         !.........||||                     ||||
+         !.........||||                     ||||
+         !.........||||     jsph.online      ||||
+         !.........||||                     ||||
+         !.........||||                     ||||
+         !.........||||                     ||||
+         '.........||||                    ,||||
+          .;.......||||               _.-!!|||||
+   .,uodWBBBBb.....||||       _.-!!|||||||||!:'
+!YBBBBBBBBBBBBBBb..!|||:..-!!|||||||!iof68BBBBBb....
+!..YBBBBBBBBBBBBBBb!!||||||||!iof68BBBBBBRPFT?!::   '.
+!....YBBBBBBBBBBBBBBbaaitf68BBBBBBRPFT?!:::::::::     '.
+!......YBBBBBBBBBBBBBBBBBBBRPFT?!::::::;:!^"';:::       '.
+!........YBBBBBBBBBBRPFT?!::::::::::^''...::::::;         iBBbo.
+'..........YBRPFT?!::::::::::::::::::::::::;iof68bo.      WBBBBbo.
+  '..........:::::::::::::::::::::::;iof688888888888b.     'YBBBP^'
+    '........::::::::::::::::;iof688888888888888888888b.     '
+      '......:::::::::;iof688888888888888888888888888888b.
+        '....:::;iof688888888888888888888888888888888899fT!
+          '..::!8888888888888888888888888888888899fT|!^"'
+            ' !!988888888888888888888888899fT|!^"'
+                '!!8888888888888888899fT|!^"'
+                  '!988888888899fT|!^"'
+                    '!9899fT|!^"'
+                      '!^"'
+                     `}
+        </code>
+      </pre>
     </section>
   );
 };
