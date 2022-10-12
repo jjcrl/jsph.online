a8863813704b5c273f98aadf21c79bfac92cf3d2
jjcrl <j412cff@icloud.com>
2022-10-12 10:31:23 +0100

    big fix for width

--- components/showcase.js
+++ components/showcase.js
@@ -50,13 +50,13 @@ const ShowCase = ({ items }) => {
           data-scroll-sticky
           data-scroll-target="#target"
         >
-          <div className="flex flex-col h-screen p-20 pb-5 justify-end ">
-            <h5 className="text-white mix-blend-difference">
+          <div className="flex flex-col h-screen p-20 pb-5 justify-end w-full">
+            <h6 className="text-white mix-blend-difference">
               {items[index].title}
-            </h5>
-            <h4 className="text-white mix-blend-difference">
+            </h6>
+            <h5 className="text-white mix-blend-difference">
               {items[index].description}
-            </h4>
+            </h5>
             <ul className="flex flex-row text-pale mix-blend-difference text-xs gap-3 italic my-5">
               {items[index].tags
                 ? items[index].tags.map((tag, index) => (
@@ -87,4 +87,3 @@ const ShowCase = ({ items }) => {
 };
 
 export default ShowCase;
-5;
