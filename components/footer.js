c097b16178211596ded0e6167f34a18b91f937fa
jjcrl <j412cff@icloud.com>
2022-10-13 12:06:16 +0100

    styling to footer done

--- components/footer.js
+++ components/footer.js
@@ -1,5 +1,4 @@
 import { React, useState, useEffect } from "react";
-import Clock from "./clock";
 
 const Footer = () => {
   const first_text = "Say hello!";
@@ -38,21 +37,33 @@ const Footer = () => {
 
   return (
     <>
-      <footer data-scroll-section className="w-screen h-min">
+      <footer data-scroll-section className="w-screen h-min bg-stone-100">
         <div className="flex flex-row w-full justify-between">
           <div className="h-full flex flex-col gap-3 px-10 pt-10 text-boyblue">
             <h4 id="blinking-cursor">{text}</h4>
             <h4 className="underline">hello@jsph.online</h4>
           </div>
-          <div className="w-fit px-10 pt-10">
-            <Clock />
+
+          <h1 className="relative top-20 left-36 text-stone-200">☺</h1>
+
+          <div className=" flex flex-row m-auto gap-16 pt-12 mr-10">
+            <div className="flex flex-col gap-3 font-bold text-boyblue">
+              <h6 className="underline">Stay Social</h6>
+              <h5>Linkedin ↗</h5>
+              <h5>Github ↗</h5>
+            </div>
+
+            <div className="flex flex-col gap-3 font-bold text-boyblue">
+              <h6 className="underline">Find me at</h6>
+              <h5>53.4875°N.</h5>
+              <h5>02.2901°W</h5>
+            </div>
           </div>
         </div>
 
-        <div className="flex flex-row justify-between w-screen px-10 text-neutral-500 ">
+        <div className="flex flex-row justify-between w-screen px-10 text-stone-200 pt-5">
           <u>©2022</u>
-          <u>jsph.online</u>
-          <u>Joseph Carroll</u>
+          <u>JSPH.ONLINE</u>
         </div>
       </footer>
     </>
