cd604b4a27659a13ef773f5dd7dfc10a71b6f646
jjcrl <j412cff@icloud.com>
2022-10-17 21:43:12 +0100

    big fix

--- components/hero.js
+++ components/hero.js
@@ -2,61 +2,61 @@ const Hero = () => {
   return (
     <section
       data-scroll-section
-      className="flex flex-row bg-stone-100 w-screen h-screen items-center align-center justify-center sm:pt-20 sm:pr-20"
+      className="flex flex-row bg-stone-100 w-screen h-screen items-center align-center justify-center sm:pt-20 sm:pr-20 pr-12"
     >
-      <pre className="text-cod text-xxs tracking-normal leading-normal sm:hidden mix-blend-difference">
+      <pre className="text-cod text-xxs tracking-normal leading-normal sm:hidden">
         <code>
           {`
-                      .,,uod8B8bou,,..
-              ..,uod8BBBBBBBBBBBBBBBBRPFT?l!i:..
-         ,=m8BBBBBBBBBBBBBBBRPFT?!||||||||||||||||
-         !...:!TVBBBRPFT||||||||||!!^^"'''''''||||
-         !.......:!?|||||!!^^""'          ||||
-         !.........||||                ||||
-         !.........||||                ||||
-         !.........||||                ||||
-         !.........|||                 ||||
-         !.........||||                ||||
-         !.........||||                ||||
-         '.........||||               ,||||
-          .;.......|||            __.!!|||||
-   .,uodWBBBBb.....||||||       _.-!!|||||||||!:'
-!YBBBBBBBBBBBBBBb..!||||||:..-!!|||||||!iof68BBBBBb
-!..YBBBBBBBBBBBBBBb!!||||||||!iof68BBBBBBRPFT?!:::....,
-!....YBBBBBBBBBBBBBBbaaitf68BBBBBBRPFT?!::::::::::    '.
-!......YBBBBBBBBBBBBBBBBBBBRPFT?!::::::;:!^"';::::      '.
-!........YBBBBBBBBBBRPFT?!::::::::::^''...::::::;;.       iBBbo.
-'..........YBRPFT?!::::::::::::::::::::::::;iof68bo.      WBBBBbo.
-  '..........:::::::::::::::::::::::;iof688888888888b.     'YBBBP^'
-    '........::::::::::::::::;iof688888888888888888888b.     '
-      '......:::::::::;iof688888888888888888888888888888b.
-        '....:::;iof688888888888888888888888888888888899fT!
-          '..::!8888888888888888888888888888888899fT|!^"'
-            ' !!988888888888888888888888899fT|!^"'
-                '!!8888888888888888899fT|!^"'
-                  '!988888888899fT|!^"'
-                    '!9899fT|!^"'
-                      '!^"'
-                     `}
+                             .,,uod8B8bou,,.
+                      ..,uod8BBBBBBBBBBBBBBBBRPFT?l!i:
+                 ,=8BBBBBBBBBBBBBBBRPFT?!|||||||||||||||
+                 !...:!TVBBBRPFT|||||||||!!^^"'   '||||
+                 !.......:!|||||!!^^"           ||||
+                 !.........||||               ||||
+                 !.........||||               ||||
+                 !.........||||               ||||
+                 !.........||||               ||||
+                 !.........||||               ||||
+                 !.........||||               ||||
+                 !.........||||              ,||||
+                 .;.......||||           _.-!!||||||
+           .,uodWBBBBb.......||||     _.-!!|||||||||!!:'
+        !YBBBBBBBBBBBBBBb...!|||:..-!!|||||||!iof68BBBBBb....
+        !..YBBBBBBBBBBBBBBb!!||||||||!iof68BBBBBBRPFT?!::   '.
+        !....YBBBBBBBBBBBBBBbaaitf68BBBBBBRPFT?!:::::::::     '.
+        !......YBBBBBBBBBBBBBBBBBBBRPFT?!::::::;:!^"';:::       '.
+        !........YBBBBBBBBBBRPFT?!::::::::::^''...::::::;         iBBbo.
+        !..........YBRPFT?!::::::::::::::::::::::::;iof68bo.      WBBBBbo.
+          '..........:::::::::::::::::::::::;iof688888888888b.     'YBBBP^'
+            '........::::::::::::::::;iof688888888888888888888b.     
+              '......:::::::::;iof688888888888888888888888888888b.
+                '....:::;iof688888888888888888888888888888888899fT!
+                  '..::!8888888888888888888888888888888899fT|!^"'
+                    '' !!988888888888888888888888899fT|!^"'
+                        '!!8888888888888888899fT|!^"'
+                          '!988888888899fT|!^"'
+                           '!9899fT|!^"'
+                              '!^"'
+                    `}
         </code>
       </pre>
-      <pre className="text-cod tracking-normal leading-normal sm:text-sm hidden sm:block">
+      <pre className="text-cod tracking-none sm:text-sm hidden sm:block">
         <code>
           {`
                               .,,uod8B8bou,,.
-                       ..,uod8BBBBBBBBBBBBBBBBRPFT?l!i:
-                  ,=m8BBBBBBBBBBBBBBBRPFT?!|||||||||||||
-                  !...:!TVBBBRPFT|||||||||!!^^""'  '||||
-                  !.......:!?|||||!!^^""'         ||||
-                  !.........||||                ||||
-                  !.........||||                ||||
-                  !.........||||                ||||
-                  !.........||||                ||||
-                  !.........||||                ||||
-                  !.........||||                ||||
-                  !.........||||               ,||||
-                   .;.......||||           _.-!!|||||
-            .,uodWBBBBb.......||||     _.-!!|||||||||!:'
+                       ..,uod8BBBBBBBBBBBBBBBBRPFT?l!i::
+                  ,=8BBBBBBBBBBBBBBBRPFT?!|||||||||||||||
+                  !...:!TVBBBRPFT|||||||||!!^^"'   '||||
+                  !.......:!|||||!!^^"            ||||
+                  !.........||||                 ||||
+                  !.........||||                 ||||
+                  !.........||||                 ||||
+                  !.........||||                 ||||
+                  !.........||||                 ||||
+                  !.........||||                 ||||
+                  !.........||||                ,||||
+                   .;.......||||           _.-!!||||||
+            .,uodWBBBBb.......||||     _.-!!|||||||||!!:'
          !YBBBBBBBBBBBBBBb...!|||:..-!!|||||||!iof68BBBBBb....
          !..YBBBBBBBBBBBBBBb!!||||||||!iof68BBBBBBRPFT?!::   '.
          !....YBBBBBBBBBBBBBBbaaitf68BBBBBBRPFT?!:::::::::     '.
@@ -77,7 +77,7 @@ const Hero = () => {
         </code>
       </pre>
 
-      <code className="block absolute sm:bottom-3/4 sm:ml-28 sm:mt-6 text-boyblue ml-8 text-xxs sm:text-sm top-64 sm:-rotate-4 -rotate-6">
+      <code className="block absolute sm:ml-28 sm:mt-6 -mt-40 text-boyblue ml-20 text-xxs sm:text-sm sm:top-64 sm:-rotate-4 -rotate-6">
         jsph.online
       </code>
     </section>
