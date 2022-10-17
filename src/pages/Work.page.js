09a53a5144db72904da78a367e42f4f3db05eb21
jjcrl <j412cff@icloud.com>
2022-10-17 20:50:18 +0100

    added differernt presentation for mobile

--- src/pages/Work.page.js
+++ src/pages/Work.page.js
@@ -1,63 +1,73 @@
-import forecastface from "../../public/images/forecastface-image2.png";
-import ncnews from "../../public/images/ncnews-image2.png";
+import forecastface from "../../public/static/images/forecastface-image2.png";
+import ncnews from "../../public/static/images/ncnews-image2.png";
+import forecastfacemini from "../../public/static/images/forecastface-mini.png";
+import ncnewsmini from "../../public/static/images/ncnews-mini.png";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function Work() {
   return (
     <>
-      <section
-        data-scroll-section
-        className="flex flex-col h-fit w-screen hidden"
-      >
-        <div className="block w-screen h-screen relative">
-          <div className="inset-0 w-screen h-screen">
+      <section data-scroll-section className="flex flex-col h-full w-screen">
+        <div className="block w-screen h-full relative sm:h-screen flex items-end">
+          <div className="inset-0 w-screen h-screen hidden sm:block">
             <Image src={ncnews} />
+          </div>
+          <div className="inset-0 w-screen h-full sm:hidden">
+            <Image src={ncnewsmini} />
           </div>
           <Link href={"/work/NcNews"}>
             <div
-              className="text-white flex flex-col h-screen w-screen justify-end items-start py-5 bottom-0 absolute"
+              className="flex flex-col sm:h-screen w-screen justify-end items-start sm:p-10 sm:bottom-0 absolute text-white h-fit p-5 sm:m-0 -mt-5 bg-blurple"
               id="works1"
             >
-              <h4 className="px-10 pb-5">Northcoders News</h4>
+              <h3 className="font-medium sm:text-5xl text-xl">
+                Northcoders News
+              </h3>
             </div>
           </Link>
         </div>
-        <div className="block w-screen h-screen relative">
-          <div className="inset-0 w-screen h-screen">
+        <div className="block w-screen h-full relative sm:h-screen flex items-end">
+          <div className="inset-0 w-screen h-screen sm:block hidden">
             <Image src={forecastface} />
+          </div>
+          <div className="inset-0 w-screen h-full sm:hidden">
+            <Image src={forecastfacemini} />
           </div>
 
           <Link href={"/work/ForecastFace"}>
             <div
-              className="text-white flex flex-col h-screen w-screen justify-end items-start py-5 bottom-0 absolute"
+              className="flex flex-col sm:h-screen w-screen justify-end items-start sm:p-10 sm:bottom-0 absolute text-white h-fit p-5 sm:m-0 bg-road"
               id="works2"
             >
-              <h4 className="px-10 pb-5">Forecast Face</h4>
+              <h3 className="font-medium sm:text-5xl text-xl">Forecast Face</h3>
             </div>
           </Link>
         </div>
       </section>
 
       <style jsx>{`
-        #works1 {
-          opacity: 0;
-          transition: opacity 2s linear 1ms;
-        }
+        @media (min-width: 640px) {
+          #works1 {
+            opacity: 0;
+            transition: opacity 1s linear;
+          }
 
-        #works1:hover {
-          opacity: 1;
-          background: rgba(0, 0, 0, 0.5);
-        }
+          #works1:hover {
+            opacity: 1;
+            background: rgba(0, 0, 0, 0.5);
+          }
 
-        #works2 {
-          opacity: 0;
-          transition: opacity 2s linear 1ms;
-        }
+          #works2 {
+            opacity: 0;
+            transition: opacity 1s linear;
+            background: transparent;
+          }
 
-        #works2:hover {
-          opacity: 1;
-          background: rgba(0, 0, 0, 0.5);
+          #works2:hover {
+            opacity: 1;
+            background: rgba(0, 0, 0, 0.5);
+          }
         }
       `}</style>
     </>
