e19b1e197e84fc53b05f390ef239c5e889053be5
jjcrl <j412cff@icloud.com>
2022-10-17 20:50:35 +0100

    static file change

--- src/pages/Home.page.js
+++ src/pages/Home.page.js
@@ -5,9 +5,9 @@ import Footer from "../../components/footer";
 import NcNewsShowcase from "../../components/ncnews.showcase";
 import TempHandSVG from "../../components/svg/temp-hand.svg";
 import TaskitFront from "../../components/TaskItFront";
-import NcNewsMini from "../../public/images/ncnews-mini.png";
-import ForecastFaceMini from "../../public/images/forecastface-mini.png";
-import TaskItMini from "../../public/images/taskit-mini.png";
+// import NcNewsMini from "../../public/images/ncnews-mini.png";
+// import ForecastFaceMini from "../../public/images/forecastface-mini.png";
+// import TaskItMini from "../../public/images/taskit-mini.png";
 
 export default function Home() {
   return (
@@ -28,7 +28,7 @@ export default function Home() {
             description: "Full Stack web development.",
             link: "www.jjcrl-nc-news.netlify.app",
             image: <NcNewsShowcase />,
-            mini_image: "/../public/images/ncnews-mini.png",
+            mini_image: "/../public/static/images/ncnews-mini.png",
             id: "showcase-item-0",
             wordsclass: "w-1/2 h-screen bg-blurple",
             image_class:
@@ -40,7 +40,7 @@ export default function Home() {
             description: "Weather data visualisation",
             link: "www.forecast-face.netlify.app",
             image: <TempHandSVG />,
-            mini_image: "/../public/images/forecastface-mini.png",
+            mini_image: "/../public/static/images/forecastface-mini.png",
             id: "showcase-item-1",
             wordsclass: "w-1/2 h-screen bg-road",
             image_class: "h-fit w-full p-5 bg-road -mt-5 z-10 text-white gap-1",
@@ -51,7 +51,7 @@ export default function Home() {
             description: "Task managment web app.",
             link: "www.jjcrl-task-managment.netlify.app",
             image: <TaskitFront />,
-            mini_image: "/../public/images/taskit-mini.png",
+            mini_image: "/../public/static/images/taskit-mini.png",
             id: "showcase-item-2",
             wordsclass: "w-1/2 h-screen bg-lime-500",
             image_class:
@@ -63,9 +63,9 @@ export default function Home() {
       <WordsSection
         type={2}
         section1="Want to know more?"
-        section2="This is some text to fill out the first section, it will be around this long."
-        section3="This will be around this long"
-        section4="And this will be the link"
+        section2="All of my prjects utilise github for versoin controll."
+        section3="So you can check out the repo's for all of the code and have a good snoop around."
+        section4="www.github.com/jjcrl"
       />
 
       <Footer />
