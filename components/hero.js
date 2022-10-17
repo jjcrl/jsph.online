9032fcf374a0f25e8f04690c62c4b871548e99a2
jjcrl <j412cff@icloud.com>
2022-10-17 20:49:32 +0100

    added extra for mbole styling

--- components/hero.js
+++ components/hero.js
@@ -2,9 +2,9 @@ const Hero = () => {
   return (
     <section
       data-scroll-section
-      className="flex flex-col bg-stone-100 w-screen h-full bg-stone-100 items-center align-center justify-center bg-boyblue py-36 gap-20 sm:py-16"
+      className="flex flex-row bg-stone-100 w-screen h-screen items-center align-center justify-center sm:pt-20 sm:pr-20"
     >
-      <pre className="text-white text-xxs tracking-normal leading-normal sm:text-xs mix-blend-difference sm:hidden">
+      <pre className="text-cod text-xxs tracking-normal leading-normal sm:hidden mix-blend-difference">
         <code>
           {`
                       .,,uod8B8bou,,..
@@ -40,47 +40,46 @@ const Hero = () => {
                      `}
         </code>
       </pre>
-      <pre className="text-white text-xxs tracking-normal leading-normal sm:text-xs mix-blend-difference hidden sm:block">
+      <pre className="text-cod tracking-normal leading-normal sm:text-sm hidden sm:block">
         <code>
           {`
-                      .,,uod8B8bou,,..
-              ..,uod8BBBBBBBBBBBBBBBBRPFT?l!i:..
-         ,=m8BBBBBBBBBBBBBBBRPFT?!||||||||||||||||
-         !...:!TVBBBRPFT||||||||||!!^^"'''''''||||
-         !.......:!?|||||!!^^""'           ||||
-         !.........||||                  ||||
-         !.........||||                  ||||
-         !.........||||                  ||||
-         !.........||||                  ||||
-         !.........||||                  ||||
-         !.........||||                  ||||
-         '.........||||                 ,||||
-          .;.......||||             __.!!|||||
-   .,uodWBBBBb.....|||||        _.-!!|||||||||!:'
-!YBBBBBBBBBBBBBBb..!||||:..-!!|||||||!iof68BBBBBb
-!..YBBBBBBBBBBBBBBb!!||||||||!iof68BBBBBBRPFT?!:::....,
-!....YBBBBBBBBBBBBBBbaaitf68BBBBBBRPFT?!::::::::::    '.
-!......YBBBBBBBBBBBBBBBBBBBRPFT?!::::::;:!^"';::::      '.
-!........YBBBBBBBBBBRPFT?!::::::::::^''...::::::;;.       iBBbo.
-'..........YBRPFT?!::::::::::::::::::::::::;iof68bo.      WBBBBbo.
-  '..........:::::::::::::::::::::::;iof688888888888b.     'YBBBP^'
-    '........::::::::::::::::;iof688888888888888888888b.     '
-      '......:::::::::;iof688888888888888888888888888888b.
-        '....:::;iof688888888888888888888888888888888899fT!
-          '..::!8888888888888888888888888888888899fT|!^"'
-            ' !!988888888888888888888888899fT|!^"'
-                '!!8888888888888888899fT|!^"'
-                  '!988888888899fT|!^"'
-                    '!9899fT|!^"'
-                      '!^"'
+                              .,,uod8B8bou,,.
+                       ..,uod8BBBBBBBBBBBBBBBBRPFT?l!i:
+                  ,=m8BBBBBBBBBBBBBBBRPFT?!|||||||||||||
+                  !...:!TVBBBRPFT|||||||||!!^^""'  '||||
+                  !.......:!?|||||!!^^""'         ||||
+                  !.........||||                ||||
+                  !.........||||                ||||
+                  !.........||||                ||||
+                  !.........||||                ||||
+                  !.........||||                ||||
+                  !.........||||                ||||
+                  !.........||||               ,||||
+                   .;.......||||           _.-!!|||||
+            .,uodWBBBBb.......||||     _.-!!|||||||||!:'
+         !YBBBBBBBBBBBBBBb...!|||:..-!!|||||||!iof68BBBBBb....
+         !..YBBBBBBBBBBBBBBb!!||||||||!iof68BBBBBBRPFT?!::   '.
+         !....YBBBBBBBBBBBBBBbaaitf68BBBBBBRPFT?!:::::::::     '.
+         !......YBBBBBBBBBBBBBBBBBBBRPFT?!::::::;:!^"';:::       '.
+         !........YBBBBBBBBBBRPFT?!::::::::::^''...::::::;         iBBbo.
+         !..........YBRPFT?!::::::::::::::::::::::::;iof68bo.      WBBBBbo.
+           '..........:::::::::::::::::::::::;iof688888888888b.     'YBBBP^'
+             '........::::::::::::::::;iof688888888888888888888b.     
+               '......:::::::::;iof688888888888888888888888888888b.
+                 '....:::;iof688888888888888888888888888888888899fT!
+                   '..::!8888888888888888888888888888888899fT|!^"'
+                     '' !!988888888888888888888888899fT|!^"'
+                         '!!8888888888888888899fT|!^"'
+                           '!988888888899fT|!^"'
+                             '!9899fT|!^"'
+                               '!^"'
                      `}
         </code>
       </pre>
-      <div>
-        <h4 className="m-auto leading-none pr-8 text-white mix-blend-difference font-medium tracking-widest opacity-40">
-          jsph.online
-        </h4>
-      </div>
+
+      <code className="block absolute sm:bottom-3/4 sm:ml-28 sm:mt-6 text-boyblue ml-8 text-xxs sm:text-sm top-64 sm:-rotate-4 -rotate-6">
+        jsph.online
+      </code>
     </section>
   );
 };
